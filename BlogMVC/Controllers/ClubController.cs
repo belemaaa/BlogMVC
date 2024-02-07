069d5766a9ed916401c063619d25b33d5ba67775
belemaaa <graceitamunoala@gmail.com>
2024-02-07 14:22:53 +0100

    created some controllers and views. Retrieved Ckub data from the db using ApplicationDbContext

---
+++ BlogMVC/Controllers/ClubController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BlogMVC.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ BlogMVC/Controllers/RaceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BlogMVC.Controllers
+{
+    public class RaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+

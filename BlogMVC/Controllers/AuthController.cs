c305bdfbe85208a8a7020218b075d70ee5fa648d
belemaaa <graceitamunoala@gmail.com>
2024-02-12 11:30:01 +0100

    login authentication

---
+++ BlogMVC/Controllers/AuthController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogMVC.Data;
+using BlogMVC.Models;
+using BlogMVC.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogMVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AuthController(ApplicationDbContext context, UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            this._context = context;
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Invalid input data");
+                return View(loginVM);
+            }
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Club");
+                    }
+                }
+                else
+                {
+                    //ModelState.AddModelError("", "Invalid credentials");
+                    TempData["Error"] = "Invalid credentials";
+                    return View(loginVM);
+                }
+            }
+            //ModelState.AddModelError("", "Invalid credentials");
+            TempData["Error"] = "Invalid credentials. Please try again";
+            return View(loginVM);
+            
+        }
+    }
+}
+
--- BlogMVC/Data/ApplicationDbContext.cs
+++ BlogMVC/Data/ApplicationDbContext.cs
@@ -16,6 +16,7 @@ namespace BlogMVC.Data
 		public DbSet<Club> Clubs { get; set; }
 
         public DbSet<Address> Addresses { get; set; }
+
     }
 }
 
